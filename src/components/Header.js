2fc861df557f8896c7f601867e5c3497d67288ed
nagesh bedge <nageshwarbedge17@gmail.com>
2025-01-13 02:02:39 +0530

    Added icons to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaHome, FaShoppingCart } from "react-icons/fa"; // Importing icons
 
 const Header = ({ cartCount }) => {
   return (
     <header className="header">
-      <h1>E-Commerce</h1>
+      <h1>
+        <FaShoppingCart className="header-icon" /> E-Commerce
+      </h1>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/">
+          <FaHome className="nav-icon" /> Home
+        </Link>
         <Link to="/cart">
-          Cart <span>({cartCount})</span>
+          <FaShoppingCart className="nav-icon" /> Cart <span>({cartCount})</span>
         </Link>
       </nav>
     </header>
