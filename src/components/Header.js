72bb43ef032f552339a8e21b16973e54a6f45bf2
nagesh bedge <nageshwarbedge17@gmail.com>
2025-01-13 03:27:08 +0530

    changed background color of cards

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,9 +14,11 @@ const Header = ({ cartCount }) => {
       <h1>
         <FaShoppingCart className="header-icon" /> E-Commerce
       </h1>
+      {/* Menu toggle button */}
       <button className="menu-toggle" onClick={toggleMenu}>
         {menuOpen ? <FaTimes /> : <FaBars />}
       </button>
+      {/* Navigation links */}
       <nav className={`nav-links ${menuOpen ? "open" : ""}`}>
         <Link to="/" onClick={() => setMenuOpen(false)}>
           <FaHome className="nav-icon" /> Home
