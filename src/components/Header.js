b1ac9c000734fc3e443a74888d77b6252f6cf223
nagesh bedge <nageshwarbedge17@gmail.com>
2025-01-12 23:48:40 +0530

    changed name of web app

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = ({ cartCount }) => {
   return (
     <header className="header">
-      <h1>E-Commerce App</h1>
+      <h1>E-Commerce</h1>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/cart">
